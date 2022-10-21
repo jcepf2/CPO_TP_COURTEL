668e7a0ac47ee9f21b2e6989544a09482f12b0c1
jules courtel <jules.courtel@epfedu.fr>
2022-10-21 17:07:37 +0200

    Avancement jusqu'à la question 12

--- TP2_Bieres_COURTEL/src/tp2_bieres_courtel/TP2_Bieres_COURTEL.java
+++ TP2_Bieres_COURTEL/src/tp2_bieres_courtel/TP2_Bieres_COURTEL.java
@@ -17,19 +17,30 @@ public class TP2_Bieres_COURTEL {
     
         BouteilleBiere uneBiere = new BouteilleBiere(" Cuvee des trolls ",7.0," Dubuisson " ) ;
         uneBiere.lireEtiquette() ;
+        uneBiere.Decapsuler() ;
         
         BouteilleBiere unedeuxiemeBiere = new BouteilleBiere(" Leffe ", 6.6, " Abbaye de Leffe " ) ;
         unedeuxiemeBiere.lireEtiquette() ;
+        unedeuxiemeBiere.Decapsuler() ;
         
-        BouteilleBiere une3Biere = new BouteilleBiere( "Heineken", 5.0, "Cuvée Heineken");
+        BouteilleBiere une3Biere = new BouteilleBiere( "Heineken", 5.0, "Cuvee Heineken");
         une3Biere.lireEtiquette() ;
+        une3Biere.Decapsuler() ;
         
         BouteilleBiere une4Biere = new BouteilleBiere("Chouffe", 8.0, "Chouffe");
         une4Biere.lireEtiquette() ;
+        une4Biere.Decapsuler() ;
                 
         BouteilleBiere une5Biere = new BouteilleBiere("Cap dona", 7.0, "Cap Dona");
         une5Biere.lireEtiquette() ;
+        une5Biere.Decapsuler() ;
         
+        system.out.println(uneBiere) ; 
+        
+        boolean test ;
+        test = uneBiere.Décapsuleur():
+        system.out.println(uneBiere)
     }
+    
     
 }
