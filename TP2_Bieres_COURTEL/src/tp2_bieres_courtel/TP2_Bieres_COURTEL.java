e4750c8a4b12c10de72caa02cefb4346ef67dbb5
jules courtel <jules.courtel@epfedu.fr>
2022-10-21 16:21:55 +0200

    Création d'un nouveau projet versionné

---
+++ TP2_Bieres_COURTEL/src/tp2_bieres_courtel/TP2_Bieres_COURTEL.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp2_bieres_courtel;
+
+/**
+ *
+ * @author jules
+ */
+public class TP2_Bieres_COURTEL {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    
+        BouteilleBiere uneBiere = new BouteilleBiere(" Cuvée des trolls ",7.0," Dubuisson " ) ;
+        uneBiere.lireEtiquette() ;
+        
+        BouteilleBiere unedeuxiemeBiere = new BouteilleBiere(" Leffe ", 6.6, " Abbaye de Leffe " ) ;
+        unedeuxiemeBiere.lireEtiquette() ;
+    }
+    
+}
