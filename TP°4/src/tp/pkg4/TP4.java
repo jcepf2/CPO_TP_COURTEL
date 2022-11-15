f46f93b025cdc8eefefbdf5759e9ea3252c8e913
jules courtel <jules.courtel@epfedu.fr>
2022-11-15 10:42:38 +0100

    TP°3 - 30 questions finies

--- TP°4/src/tp/pkg4/TP4.java
+++ TP°4/src/tp/pkg4/TP4.java
@@ -4,6 +4,9 @@
  */
 package tp.pkg4;
 
+import Personnages.Personnage;
+import Personnages.Guerrier;
+import Personnages.Magicien;
 import Armes.épée;
 import Armes.bâton;
 import Armes.Arme;
@@ -38,15 +41,34 @@ public class TP4 {
     n_Arme.add (bâton2);
     
     int taille = n_Arme.size();
-    int longueur = taille - 1;
-    for (int i=0; i<longueur; i++) {
+    int longueur1 = taille - 1;
+    for (int i=0; i<longueur1; i++) {
         System.out.println(n_Arme.get(i)+", ");
     }
     
 System.out.println(n_Arme.get(taille-1));
     
+    Magicien magicien1 = new Magicien(" Gandalf ",65, true) ;
+    Magicien magicien2 = new Magicien(" Garcimore ",44,false) ;
+        
+    Guerrier guerrier1 = new Guerrier(" Conan ",78,true) ;
+    Guerrier guerrier2 = new Guerrier(" Lannister ",45, false ) ;
+           
+    System.out.println(magicien1);
+    System.out.println(magicien2);
+    System.out.println(guerrier1);
+    System.out.println(guerrier2);
     
+    ArrayList <Personnage> n_Personnage = new ArrayList <Personnage>();
+    n_Personnage.add (magicien1);
+    n_Personnage.add (magicien2);
+    n_Personnage.add (guerrier1);
+    n_Personnage.add (guerrier2);
     
+    int distance = n_Personnage.size();
+    int longueur2 = distance - 1;
+    for (int i=0; i<longueur2; i++) {
+        System.out.println(n_Personnage.get(i)+", ");
     }
-    
+    }
 }
